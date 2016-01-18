6f1034dfacd81e75086d8b83a7721cedc5c8ce47
Student <student@RU-102.local>
2016-01-18 11:12:36 -0700

    namePrompt

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('Hello Boulder!')
+function namePrompt () {
+	var name = prompt("Please enter your name", "Enter Name Here");
+
+	if (name != null) {
+		document.getElementById("demo").innerHTML =
+		"Hello " + name + "! how you doin?";
+	}
+}
