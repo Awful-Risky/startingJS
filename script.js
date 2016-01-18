57d6c02cc5b558d43bc8714bea7c2efadfcd220e
Student <student@RU-102.local>
2016-01-18 11:42:20 -0700

    namePrompt, loops

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function namePrompt () {
 		"Hello " + name + "! how you doin?";
 	}
 }
+
+
+for(var i = 0; i <= 10; i++) {
+	console.log(i);
+}
+
+for(var i = 0; i <= 500; i+=100) {
+	console.log(i);
+}
+
+for(var i = 1; i <= 64; i*=2) {
+	console.log(i);
+}
+
+for(var i = 0; i <= 10; i+=2) {
+	console.log(i);
+}
+
+for(var i = 1; i < 4; i++) {
+	console.log(i);
+	console.log(i);
+	console.log(i);
+}
+
+for(var i = 3; i <= 15; i+=3) {
+	console.log(i);
+}
